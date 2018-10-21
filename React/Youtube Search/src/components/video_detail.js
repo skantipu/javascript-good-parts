9ba7d094e10d77e1e37d2a8783176c3287555531
Sachin <skantipu@eng.ucsd.edu>
2018-10-21 16:08:43 -0700

    react

--- React/Youtube Search/src/components/video_detail.js
+++ React/Youtube Search/src/components/video_detail.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 export default ({ video }) => {
-  if (video) {
-    const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
-    console.log(videoURL);
-    return (
-      <div>
-        <iframe src={videoURL} frameBorder='0' allow='autoplay; encrypted-media' allowFullScreen></iframe>
-        <div className='video-detail-desc'>
-          <div>{video.snippet.title}</div>
-          <div>{video.snippet.description}</div>
-        </div>
-      </div>
-    );
-  } else {
-    return <div></div>;
+  if (!video) {
+    return <div>Loading...</div>;
   }
+  const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
+  console.log(videoURL);
+  return (
+    <div>
+      <iframe src={videoURL} frameBorder='0' allow='autoplay; encrypted-media' allowFullScreen></iframe>
+      <div className='video-detail-desc'>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
 }

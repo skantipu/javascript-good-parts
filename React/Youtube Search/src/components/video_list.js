15a0d8ef5264ac0acec331f53190dd02ff2b192a
Sachin <skantipu@eng.ucsd.edu>
2018-10-21 14:39:43 -0700

    react

--- React/Youtube Search/src/components/video_list.js
+++ React/Youtube Search/src/components/video_list.js
@@ -2,8 +2,8 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 export default (props) => {
-  const videoItemsList = props.videos.map(video => {
-    return <VideoListItem video={video}/>
+  const videoItemsList = props.videos.map((video, i) => {
+    return <VideoListItem video={video} key={i}/>
   });
   return (
     <ul>

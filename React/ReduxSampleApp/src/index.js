aff9492b5f45eb07c05fbf2f3b2cb7a889fae394
Sachin <skantipu@eng.ucsd.edu>
2018-10-23 11:00:40 -0700

    redux

---
+++ React/ReduxSampleApp/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style/style.scss';
+
+const App = () => {
+  const title = 'Hello World!!';
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ React/ReduxSampleApp/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};

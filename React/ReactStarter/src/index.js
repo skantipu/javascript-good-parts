a82d496af7995b36537181adf9f073492fe71f48
Sachin <skantipu@eng.ucsd.edu>
2018-10-23 10:54:44 -0700

    react starter kit

--- React/ReactStarter/src/index.js
+++ React/ReactStarter/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import '../style/style.scss';
 
 const App = () => {
-  const title = 'Hello World!!'
+  const title = 'Hello World!!';
   return (
     <div>
       <h1>{title}</h1>
     </div>
   );
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));

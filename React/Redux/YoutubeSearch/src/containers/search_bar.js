49e027f37de832a509d715fbe7dd4edfcb09e1e0
Sachin <skantipu@eng.ucsd.edu>
2018-10-28 07:47:01 -0700

    redux

--- React/Redux/YoutubeSearch/src/containers/search_bar.js
+++ React/Redux/YoutubeSearch/src/containers/search_bar.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { bindActionCreators } from 'redux';
-import { updatedVideos } from '../actions/index';
+import { updatedVideos, videoSelected } from '../actions/index';
 import YTSearch from 'youtube-search';
 const key = 'AIzaSyDJ23y4_hQMa39dFM-1yxyT_RWgetg0yXk';
 
 class SearchBar extends Component {
-  onInputChange(e) {
-    const term = e.target.value;
+  constructor(props) {
+    super(props);
+    this.state = { term: 'ayurveda' };
+    this.searchVideos(this.state.term);
+    this.debounceTermChange = _.debounce(term => { this.searchVideos(term)}, 1000);
+  }
+  searchVideos(term) {
     YTSearch(term, { maxResults: 10, key }, (err, videos) => {
       this.props.updatedVideos(videos);
+      this.props.videoSelected(videos[0]);
     });
   }
+  onInputChange(e) {
+    const term = e.target.value;
+    this.setState({ term });
+    this.debounceTermChange(term);
+    }
   render() {
     return (
       <div className="search-input-holder">
         <input type="text"
+               value={this.state.term}
                onChange={(e) => this.onInputChange(e)}
                placeholder="search"
         />
@@ -24,6 +37,6 @@ class SearchBar extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({ updatedVideos }, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ updatedVideos, videoSelected }, dispatch);
 
-export default connect(()=>{}, mapDispatchToProps)(SearchBar);
+export default connect(()=>({}), mapDispatchToProps)(SearchBar);
